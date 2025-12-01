5aa19ebb3f73e52e24ac3ed97988a3a21a22aba2
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-12-01 15:21:28 -0600

    look into various ways to keep wine bottles in racks from clipping

---
+++ datedliquor/datedliquor/src/BlockClass/BlockTallRackBottle.cs
@@ -0,0 +1,33 @@
+﻿using ACulinaryArtillery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+
+namespace datedliquor.src.BlockClass
+{
+    public class BlockTallRackBottle : BlockBottle
+    {
+        public override void OnHeldInteractStart(ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling)
+        {
+                var api = byEntity.World.Api;
+                if (api.World.BlockAccessor.GetBlock(blockSel.Position).Code.PathStartsWith("bottlerackcorner"))
+                {
+                    api.Logger.Event("Bottle Rack Slot Index: " + blockSel.SelectionBoxIndex.ToString());
+                    //blockSel.SelectionBoxIndex
+                    //beBottleRack.Inventory.
+                    if (false) 
+                    {
+                        (api as ICoreClientAPI)?.TriggerIngameError(this, "racktoocrowded", Lang.Get("datedliquor:bottlerack-slot-toocrowded"));
+                        handHandling = EnumHandHandling.NotHandled;
+                    return;
+                    }
+            }
+            base.OnHeldInteractStart(itemslot, byEntity, blockSel, entitySel, firstEvent, ref handHandling);
+        }
+    }
+}
---
+++ datedliquor/datedliquor/src/Harmony/ACABottleRackPatch.cs
@@ -0,0 +1,54 @@
+﻿using ACulinaryArtillery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using HarmonyLib;
+
+namespace datedliquor.src.Harmony
+{
+    [HarmonyPatch(typeof(BlockEntityBottleRack),"OnInteract")]
+    public static class ACABottleRackPatch
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(IPlayer byPlayer, BlockSelection blockSel, BlockEntityBottleRack __instance, ref bool __result)
+        {
+            var playerSlot = byPlayer.InventoryManager.ActiveHotbarSlot;
+            
+                var colObj = playerSlot.Itemstack.Collectible;
+
+                BlockBottle bottle = colObj as BlockBottle;
+                
+                //Check if Corner rack and bottle being placed is a winebottle
+                if(__instance.Block.Code.PathStartsWith("bottlerackcorner") && bottle.Code.PathStartsWith("winebottle"))
+                {
+                    __instance.Api.Logger.Event("blocksel:"+blockSel.SelectionBoxIndex);
+                    var index = blockSel.SelectionBoxIndex;
+
+                var sideslots = __instance.Inventory.GetSlotsIfExists(byPlayer, [__instance.Inventory.InventoryID], [index - 1, index + 1]);
+               
+                
+                if (false)
+                {
+                    __instance.Api.Logger.Event("inventory:", string.Join(",", __instance.Inventory));
+
+
+
+                    //Run fullness checks from base method
+                    float fullness = bottle?.GetCurrentLitres(playerSlot.Itemstack) ?? 0;
+                    if (bottle?.IsTopOpened == true && fullness > 0.2f)
+                    {
+                        (__instance.Api as ICoreClientAPI)?.TriggerIngameError(__instance, "bottletoofull", Lang.Get("aculinaryartillery:bottle-toofullforrack"));
+                        __result = false;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
