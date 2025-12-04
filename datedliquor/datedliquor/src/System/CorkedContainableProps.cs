2bb1a7511867fe1b6e7f3ee8201a0a1a9584b065
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-12-04 16:56:41 -0600

    add corkable container props

---
+++ datedliquor/datedliquor/src/System/CorkedContainableProps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace datedliquor.src.System
+{
+    public class CorkedContainableProps
+    {
+        //Ubject to hold all extra nexxecary attributes
+        AssetLocation CorkAddSound = new AssetLocation();
+
+        AssetLocation CorkPopSound = new AssetLocation();
+        
+
+        public bool HasDate;
+
+        public double FirstCorkedHours;
+
+        public double LastCorkedHours;
+
+    }
+}
